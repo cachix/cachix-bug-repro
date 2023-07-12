8cdd118b1a9838ec96a163a59e39c14c386ca791
Sander <hey@sandydoo.me>
2023-07-12 19:17:42 +0000

    Fixup

--- flake.nix
+++ flake.nix
@@ -1,17 +1,17 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, ... }: {
     nixosModules.vm = {config, ...}: {
       services.getty.autologinUser = "root";
 
-      environment.variables.reprodir = "<reprodir>";
+      environment.variables.reprodir = "${self}";
 
       nixos-shell.mounts.mountHome = true;
       virtualisation.memorySize = 1500;
       virtualisation.diskSize = 5000;
-      virtualisation.cores = 3;
-      virtualisation.writableStoreUseTmpfs = false;
+      virtualisation.cores = 6;
+      virtualisation.writableStoreUseTmpfs = true;
       nix.settings.experimental-features = "nix-command flakes";
 
       system.stateVersion = config.system.nixos.release;
